87b3ad0b997e48c0141f40b110c4d0dfc508a0c2
Marcin Polak <mapoart@gmail.com>
2020-01-15 20:44:37 +0100

    Added Nexss Programmer Installation Info

---
+++ templates/helloWorld.ts.js
@@ -0,0 +1,10 @@
+// additional info for templates like copy extra libraries.
+// in this case library needs JSON
+const config = {
+  files: [""],
+  commands: ["npm i @types/node"],
+  repos: [""],
+  descriptions: [""]
+};
+
+module.exports = config;
