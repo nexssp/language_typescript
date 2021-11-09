87abf0254fd9920b37f1d652ec2cc1c44482b555
Marcin Polak <mapoart@gmail.com>
2021-11-09 18:24:38 +0100

    Text error adjustment.

--- nexss.typescript.errors.js
+++ nexss.typescript.errors.js
@@ -3,7 +3,7 @@ module.exports = {
   "Cannot find module '(.*?)'": "nexss js install <package>",
   "ReferenceError: (?<found1>.*?) is not defined":
     "Define variable <found1> as it is not found. (This is an example of error solution in the Nexss Programmer. " +
-    "You can add your own one. " +
+    "You can add your own one for every languages. " +
     "For more please here: https://github.com/nexssp/cli/wiki/Errors-Solutions" +
     ")",
 };
