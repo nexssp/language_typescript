40fb4f035f68720cd712bded4dde08edc92c0e03
Marcin Polak <mapoart@gmail.com>
2023-05-07 16:15:12 +0200

    Support for Android platforms

--- typescript.android.nexss.config.js
+++ typescript.android.nexss.config.js
@@ -3,7 +3,7 @@ let languageConfig = Object.assign(
   require(`./typescript.win32.nexss.config.js`)
 );
 
-languageConfig.compilers.deno = {
+languageConfig.compilers["ts-node"] = {
   install: `npm install -g ts-node @types/node typescript`,
   command: "ts-node",
   args: "--log-error <file>",
